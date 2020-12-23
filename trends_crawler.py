9035a9ac3278949007a35e396617589497c96c37
SteveKimSR <kimsr96@naver.com>
2020-12-23 15:14:49 +0900

    clean code

--- trends_crawler.py
+++ trends_crawler.py
@@ -1,9 +1,8 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import tweepy
-import threading
-import time
 import pandas as pd
+import tweepy
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from tqdm.auto import tqdm
 
 
 class twitter:
@@ -12,20 +11,20 @@ class twitter:
     trends_count = 0
 
     def __init__(self):
-        consumer_key = 'MEkO26YccoCAZLzKnYtY9zpHZ'
-        consumer_secret = 'LMaFEAMMyblIoboDfQIEeRLcDyWNQqXPmplujLogJtXwIruSYv'
-        access_token = '1324111638673305600-7cqll3GwZHAGPRYNVqz4meL7pS3eaE'
-        access_secret = 'fZuubaUVDrBhAg3VXnVDdnoXigmdtA1YEoOoZDK253WYY'
+        consumer_key = "Your key"
+        consumer_secret = "Your key"
+        access_token = "Your key"
+        access_secret = "Your key"
 
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_secret)
 
         self.api = tweepy.API(auth, wait_on_rate_limit=True)
-        print('twitter API connected!')
+        print("twitter API connected!")
 
     def trends(self, url):
         # 여기 경로는 각자 selenium chrome path로 설정하면 됨. 동봉한 driver 참고
-        driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+        driver = webdriver.Chrome(executable_path="./chromedriver.exe")
         # selenium이 웹 자원이 로드될 때 (3초)까지 기다려줌.
         driver.implicitly_wait(3)
         # chrome 페이지 open
@@ -34,8 +33,8 @@ class twitter:
         html = driver.page_source
         # 위 코드도 가능하지만, lxml이 최근 버전에 호환이 더 잘 된다고 하고, 속도도 빠르다고 해서 lxml사용. 쓰려면 pip로 받아야 함.
         # soup = BeautifulSoup(html, 'html.parser')
-        soup = BeautifulSoup(html, 'lxml')
-        trends = soup.select('td.main > a')
+        soup = BeautifulSoup(html, "lxml")
+        trends = soup.select("td.main > a")
         # 따온 데이터를 text화 해서 각각 나타내기
         trends_arr = []
         for i in trends:
@@ -44,24 +43,38 @@ class twitter:
         # 창 닫기
         driver.close()
         return trends_arr
-        print('twitter trends crawl done!')
+        print("twitter trends crawl done!")
 
     def init_work(self, trends: list):
         print("Start working...")
-        for trend in trends:
-            t = threading.Thread(target=self.search_keyword, args=(trend,))
-            t.start()
-        while self.trends_count != len(trends):
-            time.sleep(5)
-        result = pd.DataFrame(self.result_file, columns=['keyword', 'posted_time', 'text', 'primary words'])
-        result.to_csv('result.tsv', index=False, sep="\t")
+
+        for num, trend in enumerate(trends):
+            self.search_keyword(trend)
+            result = pd.DataFrame(
+                self.result_file,
+                columns=[
+                    "keyword",
+                    "posted_time",
+                    "retweet count",
+                    "text",
+                    "primary words",
+                ],
+            )
+            result.to_csv(str(num) + ".tsv", index=False, sep="\t")
+            self.result_file = []
+
         print("Searching job done!!!")
 
     def search_keyword(self, keyword: str):
         print(f"keyword : {keyword} search start...")
-        for tweet in tweepy.Cursor(self.api.search, q=keyword, lang='ko', include_entities=False).items(10):
+
+        for tweet in tqdm(
+            tweepy.Cursor(
+                self.api.search, q=keyword, lang="ko", include_entities=False
+            ).items(1000)
+        ):
             if "RT" not in tweet.text:
                 line = [keyword, str(tweet.created_at), tweet.text]
                 self.result_file.append(line)
-        self.trends_count += 1
-        print(f"keyword : {keyword} search done!")
+
+        print(f"keyword : {keyword} search done!\n")
--- word_analysis.py
+++ word_analysis.py
@@ -7,25 +7,24 @@ Original file is located at
     https://colab.research.google.com/drive/1ugJ-O5KhO3JkRbMerCMVi10si722MxKx
 """
 
-from konlpy.tag import Hannanum
-from collections import Counter
 import re
+from collections import Counter
+
+from konlpy.tag import Hannanum
 from krwordrank.word import KRWordRank
 from nltk import Text
 from nltk.tokenize import regexp_tokenize
 
 
 def remove_special_characters(texts):
-    texts = re.sub(
-        '[-+=,#/\?:^$!@%&()~]', '', texts
-    )
+    texts = re.sub("[-+=,#/\?:^$!@%&()~]", "", texts)
     return texts
 
 
 def word_analysis(str):
 
     en = regexp_tokenize(str, "([A-Za-z]+)")
-    T=list(Text(en))
+    T = list(Text(en))
     texts = remove_special_characters(str)
     noun = Hannanum().nouns(texts)
     noun.extend(T)
@@ -48,23 +47,28 @@ def word_analysis(str):
 texts = "송고시간2020-12-22 17:38 공유 댓글 글자크기조정 인쇄 백나용 기자백나용 기자 (제주=연합뉴스) read read read 백나용 기자 = 제주도는 22일 0시부터 오후 5시까지 신종 코로나바이러스 감염증(코로나19) 신규 확진자 11명(273∼283번)이 발생했다고 밝혔다.도에 따르면 273번은 7080 라이브 카페 확진자 접촉자로, 274번은 7080 라이브 카페를 방문한 이력이 있다.이에 따라 7080 라이브 카페 관련 확진자는 지난 18일 첫 발생 이후 현재까지 33명으로 늘었다.277번 확진자는 지난 21일 확진 판정을 받은 한국국제교류재단 직원의 접촉자다.277번 확진자는 지난 21일 서울에서 제주로 입도, 제주 입도 후 제주공항 워크스루 선별진료소를 방문해 검체를 채취했다.282번 확진자는 제주 270번 확진자의 가족이다.나머지 확진자 7명에 대해서는 기초 조사가 진행 중이다.아울러 원희룡 제주지사는 이날 오전 코로나19 대책회의를 주재하고 오는 24일부터 전국 모든 식장에서 5인 이상 모임이 전면 금지되는 것과 관련해 세부 지침을 마련하라고 지시했다.특히 원 지사는 이번 조치로 제주로 입도할 예정이었던 입도객이 숙박업소에 대한 예약 취소로 위약금 부담이 발생할 것으로 보고 중앙정부에 전액 환불을 요청해 달라고도 덧붙였다.원 지사는 중앙정부의 5인 이상 집합 금지 조치에 따라 숙박업소에 대한 예약 취소가 속출하고, 이 피해는 고스란히 숙박 예약을 취소한 이들이 받고 있다며 중앙정부의 조치로 피해를 보는 부분은 중앙정부가 책임을 져야 한다고 말했다."
 # 텍스트 넣기
 
-word_list = word_analysis(texts)#텍스트 분석해서 키워드 추출
+word_list = word_analysis(texts)  # 텍스트 분석해서 키워드 추출
 print(word_list)
 
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 def show_word(word_list):
-    text = ''
+    text = ""
     for word in word_list:
         for num in range(word[1]):
-            text = text + ' ' + word[0]
-    wc = WordCloud(width=1000, height=600, background_color="white", random_state=0,
-                   font_path='MaruBuri-Regular.ttf')
+            text = text + " " + word[0]
+    wc = WordCloud(
+        width=1000,
+        height=600,
+        background_color="white",
+        random_state=0,
+        font_path="MaruBuri-Regular.ttf",
+    )
     plt.imshow(wc.generate(text))
     plt.axis("off")
     plt.show()
 
 
-show_word(word_list)#추출한 리스트를 넣으면 시각적으로 보여줌
+show_word(word_list)  # 추출한 리스트를 넣으면 시각적으로 보여줌
